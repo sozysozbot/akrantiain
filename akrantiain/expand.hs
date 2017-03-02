0a50e8324c34c952d5e7b594cc798dcd559ab667
sozysozbot <sozysozbot@gmail.com>
2017-03-03 04:29:48 +0900

    Write 'split'

--- akrantiain/expand.hs
+++ akrantiain/expand.hs
@@ -15,4 +15,9 @@ expand :: [Sentence] -> [Conv2]
 expand = undefined . split
 
 split :: [Sentence] -> (S.Set([Orthography],[Phoneme]),M.Map Identifier [Candidates])
-split = undefined
+split [] = (S.empty, M.empty)
+split (Conversion orthos phonemes : xs) = (S.insert (orthos, phonemes) s , m)
+ where (s,m) = split xs 
+split (Define ident cands : xs) = let (s,m) = split xs in 
+ if ident `M.member` m then undefined else (s, M.insert ident cands m)
+
