389d2a3c751aae0925a9b501818aca8599301f0c
sozysozbot <sozysozbot@gmail.com>
2017-03-05 01:01:27 +0900

    check the balance of left- and right-hand side of conversion

--- akrantiain/expand.hs
+++ akrantiain/expand.hs
@@ -1,4 +1,4 @@
---{-# OPTIONS -Wall -fno-warn-unused-do-bind #-}
+{-# OPTIONS -Wall -fno-warn-unused-do-bind #-}
 
 module Akrantiain.Expand
 (SemanticError(..)
@@ -12,23 +12,22 @@ import Akrantiain.Resolve_definitions
 import qualified Data.Set as S
 import qualified Data.Map as M
 import Data.Maybe(fromJust)
+import Data.List(intercalate)
 
 data Conv2 = Conv (Array Orthography') [Phoneme] deriving(Show, Eq, Ord)
 
 -- List required only for $-numbering, which will be numbering [Orthography']
 data Orthography' = Neg' [Resolved] | Pos' [Resolved] deriving(Show, Eq, Ord)
--- newtype Resolveds = R(Array Resolved)
--- newtype Candidates = C(Array Candidate) deriving(Show, Eq, Ord)
--- data Sentence = Conversion (Array Orthography) (Array Phoneme) | Define Identifier (Set Candidates) deriving(Show, Eq, Ord)
--- data Orthography = Neg Candidate | Pos Candidate deriving(Show, Eq, Ord)
--- data Phoneme = Dollar Int | Slash String deriving(Show, Eq, Ord)
--- data Candidate = Res Resolved | Ide Identifier deriving(Show, Eq, Ord)
--- data Resolved = Boundary | Quo Quote deriving(Show, Eq, Ord)
--- newtype Identifier = Id String deriving(Show, Eq, Ord)
--- newtype Quote = Quote String deriving(Show, Eq, Ord)
+
+check_length :: Set Sentence -> Either SemanticError (Set Sentence)
+check_length sents = case [ (orthos, phonemes) | Conversion orthos phonemes <- sents, length[()|Pos _ <- orthos] /= length phonemes] of
+ [] -> return sents
+ arr  -> Left $ E{errNum = 3, errStr = "mismatched number of concrete terms in left- and right-hand side of "++ intercalate ", " ["{"++ toSource (Conversion os ps)++"}" | (os,ps) <- arr]}
+
 
 expand :: Set Sentence -> Either SemanticError (Set Conv2)
-expand sents = do 
+expand sents' = do 
+ sents <- check_length sents'
  (orthoset, identmap) <- split sents
  newMap <- candids_to_quotes identmap
  return $ do{
