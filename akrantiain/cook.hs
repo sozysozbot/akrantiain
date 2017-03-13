c7355bf14aab555527bbba0330aa885a36bbb605
sozysozbot <sozysozbot@gmail.com>
2017-03-13 13:08:41 +0900

    tttttttt

--- akrantiain/cook.hs
+++ akrantiain/cook.hs
@@ -8,11 +8,13 @@ module Akrantiain.Cook
 ,RuntimeError(..)
 ) where
 import Akrantiain.Expand
+import Akrantiain.Structure
 data RuntimeError = RE {errNo :: Int, errMsg :: String} deriving(Eq, Ord)
 instance Show RuntimeError where
  show RE{errNo = n, errMsg = str} = "Runtime error (error code #" ++ show n ++ ")\n" ++ str 
 
 data Fixme3 = Fixme3 
+data Fixme4 = Fixme4 
 type Input = String
 type Output = Either RuntimeError String
 cookBy :: Fixme3 -> Either SemanticError( Input -> Output )
@@ -23,3 +25,28 @@ cookBy foobar = undefined
    -- | x == "wa" = Right "わ"
    -- | x == "wu" = Left $ RE{errNo = 1030, errMsg = "wu is not good"}
    -- | otherwise = Right x
+
+   
+cookBy' :: Fixme4 -> Input -> Output
+cookBy' fixme4 input = undefined
+
+
+data Stat = Stat
+
+type Rule = Array (Array Resolved, Array Phoneme)
+
+update :: Rule -> Stat -> Stat
+update = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
